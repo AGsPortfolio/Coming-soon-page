219b8fbee5aa765c96792996c10a7d19f76ef6d5
AGsPortfolio <64134099+AGsPortfolio@users.noreply.github.com>
2021-10-16 15:55:29 +0200

    Coding

--- javascript.js
+++ javascript.js
@@ -2,11 +2,11 @@ function EmailValidation (enteredEmail) {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     if(enteredEmail.value.match(mailformat)) {
-        alert("Thank you for your interest in Base Apparel!");
+        document.getElementById("message").innerHTML = "Thank you for your interest in Base Apparel!";
         document.emailValidation.emailAddress.focus();
         return true;
     } else {
-        alert("Please provide a valid email address");
+        document.getElementById("message").innerHTML = "Please provide a valid email address";
         document.emailValidation.emailAddress.focus();
         return false;
     }
