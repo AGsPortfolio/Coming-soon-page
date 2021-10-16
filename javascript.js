7e614ba328aaf3fd0b66a7987ff59d9e4d24685d
AGsPortfolio <64134099+AGsPortfolio@users.noreply.github.com>
2021-10-16 17:01:57 +0200

    Pfft

--- javascript.js
+++ javascript.js
@@ -1,18 +1,13 @@
 function EmailValidation (enteredEmail) {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     
-    setTimeout(function() {
-    if(enteredEmail.value.match(mailformat)) {
+        if(enteredEmail.value.match(mailformat)) {
         document.getElementById("message").innerHTML = "Thank you for your interest in Base Apparel!";
-        document.emailValidation.emailAddress.focus();
-        return true;
     } else {
         document.getElementById("message").innerHTML = "Please provide a valid email address";
-        document.emailValidation.emailAddress.focus();
-        return false;
     }
-}, 1000);
 }
+
 
 
 
